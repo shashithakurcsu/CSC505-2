f323c00264391737f2ee164ec94b5efd1284e785
shashithakurcsu <156634085+shashithakurcsu@users.noreply.github.com>
2024-09-21 14:31:04 -0500

    hardcode data within the program - no api call

--- people_in_space.py
+++ people_in_space.py
@@ -1,28 +1,29 @@
-import requests
-
+#Mimick api with data locally
 def get_people_in_space():
-  
-    api_url = 'http://api.open-notify.org/astros.json'
-    
-    try:
-        response = requests.get(api_url)
-        data = response.json()
-        
-        # Check if the API returned an error
-        if response.status_code != 200 or 'people' not in data:
-            print("Error fetching data from the API.")
-            return
-        
-        number_of_people = data['number']
-        people = data['people']
-        
-        print(f"Total number of people in space: {number_of_people}\n")
-        print("Names of people currently in space:")
-        for person in people:
-            print(f"- {person['name']} on the {person['craft']}")
-    
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+
+    data = {
+        'number': 7,
+        'people': [
+            {'name': 'Sergey Prokopyev', 'craft': 'ISS'},
+            {'name': 'Dmitry Petelin', 'craft': 'ISS'},
+            {'name': 'Frank Rubio', 'craft': 'ISS'},
+            {'name': 'Fei Junlong', 'craft': 'Tiangong'},
+            {'name': 'Deng Qingming', 'craft': 'Tiangong'},
+            {'name': 'Zhang Lu', 'craft': 'Tiangong'},
+            {'name': 'Sultan Al Neyadi', 'craft': 'ISS'}
+        ]
+    }
+    return data
+
+def display_people_in_space(): 
+    data = get_people_in_space()
+    number_of_people = data['number']
+    people = data['people']
+
+    print(f"Total number of people in space: {number_of_people}\n")
+    print("Names of people currently in space:")
+    for person in people:
+        print(f"- {person['name']} on the {person['craft']}")
 
 if __name__ == '__main__':
-    get_people_in_space()
+    display_people_in_space()
